b82dc24f3eac54d01e2c9b31202d6986101372eb
Chris <CAlfano1999@gmail.com>
2020-05-07 16:03:09 -0400

    Latest

--- app/src/main/java/com/example/ownstoryapp/MainActivity.java
+++ app/src/main/java/com/example/ownstoryapp/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.ownstoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText userInput;
+    Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.userInput);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = userInput.getText().toString();
+                Toast.makeText(MainActivity.this,name,Toast.LENGTH_LONG).show();
+                startStory();
+            }
+        });
+    }
+    public void startStory(){
+        Intent intent = new Intent(MainActivity.this,StoryActivity.class);
+        startActivity(intent);
     }
 }
