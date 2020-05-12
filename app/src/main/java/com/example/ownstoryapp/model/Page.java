932c95aa4818648a5c7ea738788623fcc0dc5671
Chris <CAlfano1999@gmail.com>
2020-05-12 16:02:38 -0400

    Latest

--- app/src/main/java/com/example/ownstoryapp/model/Page.java
+++ app/src/main/java/com/example/ownstoryapp/model/Page.java
@@ -1,4 +1,12 @@
 package com.example.ownstoryapp.model;
 
 public class Page {
+    private int imageId;
+    private int textId;
+    private Choice choice1;
+    private Choice choice2;
+    private boolean isFinalPage = false;
+
+    //constructor
+
 }
--- app/src/main/java/com/example/ownstoryapp/model/Story.java
+++ app/src/main/java/com/example/ownstoryapp/model/Story.java
@@ -1,4 +1,5 @@
 package com.example.ownstoryapp.model;
 
 public class Story {
+
 }
